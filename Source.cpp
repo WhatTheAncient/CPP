0894045b3ee1921c0be151fa23bd804b54366cdf
WhatTheAncient <whattheanc1ent@yandex.ru>
2021-03-16 12:22:27 +0300

    Программа доработана согласно замечаниям.

--- Source.cpp
+++ Source.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
-#include <ctime>
+#include <random>
 using namespace std;
 
 
-int* array_keyboard_filling(int size) {
-	int* arr;
-	arr = new int[size];
-	for (int i = 0; i < size; i++) {
-		cout << "Enter array element ";
+int* array_keyboard_filling(const size_t array_size, const string& message) {
+	auto arr = new int[array_size];
+	for (int i = 0; i < array_size; i++) {
+		cout << message;
 		cin >> arr[i];
 	};
 	return arr;
 };
 
-int* array_random_filling(int size) {
-	int* arr;
-	srand(time(NULL));
-	arr = new int[size];
-	for (int i = 0; i < num; i++) {
-		arr[i] = rand() % 31 - 10;
+int* array_random_filling(const size_t array_size) {
+	random_device rd;
+	mt19937 gen(rd());
+	int rand_min = -10, rand_max = 20;
+	uniform_int_distribution <> dis(rand_min, rand_max);
+	auto arr = new int[array_size];
+	for (int i = 0; i < array_size; i++) {
+		arr[i] = dis(gen);
 	};
 	return arr;
 };
@@ -27,7 +28,7 @@ void array_destroyer(int* arr) {
 	delete[] arr;
 };
 
-int* replace_last_el(int* arr, int size) {
+int* replace_last_el(int* arr, const size_t size) {
 	for (int i = size - 1; i >= 0; i--) {
 		if (arr[i] < 0) {
 			arr[i] = arr[size - 2];
@@ -37,7 +38,7 @@ int* replace_last_el(int* arr, int size) {
 	return arr;
 };
 
-void del_even_numbers(int* arr, int size) {
+void del_even_numbers(int* arr, const size_t size) {
 	int new_size = 0;
 	for (int i = 0; i < size; i++) {
 		if (arr[i] % 2 != 0) {
@@ -54,10 +55,10 @@ void del_even_numbers(int* arr, int size) {
 	for (int i = 0; i < new_size; i++) {
 		cout << new_arr[i] << ", ";			
 	} 
-	
+	cout << endl;
 }
 
-int* create_array(int* arr, int size){
+int* create_array(int* arr, const size_t size){
 	int* new_arr;
 	new_arr = new int[size];
 	for (int i = 0; i < size; i++) {
@@ -71,8 +72,7 @@ int* create_array(int* arr, int size){
 	return new_arr;
 }
 
-void array_print(int* arr, int size) {
-	cout << size << endl;
+void array_print(int* arr, const size_t size) {
 	for (int i = 0; i < size; i++) {
 		cout << arr[i] << ", ";
 	};
@@ -83,14 +83,14 @@ void array_print(int* arr, int size) {
 int main() {
 	char filling_type;
 	cout << "Enter number of array elements: ";
-	int size;
+	size_t size;
 	cin >> size;
-	cout << endl << "Choose how to fill the array ('k' or 'r')";
+	cout << endl << "Choose how to fill the array ('k' or 'r'): ";
 	cin >> filling_type;
 	cout << endl;
 	int* a;
 	if (filling_type == 'k') {
-		a = array_keyboard_filling(size);
+		a = array_keyboard_filling(size, "Enter array element: ");
 	}
 	else {
 		a = array_random_filling(size);
